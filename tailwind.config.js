e906b7e5b82a6943d1933792924c7fb8c05aae70
Terence Sawtell <terence.sawtell@meetgoat.com>
2024-02-22 16:54:56 -0800

    added text and fixed logo spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./build/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        TofinoProNarrow: ['"TofinoProNarrowRegular", "sans-serif"']
+      }
+    },
   },
   plugins: [],
 }
